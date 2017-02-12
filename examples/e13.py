036208fc55f1f14e5410429274552788d7921637
Rubén Crespo-Cano <rcrespocano@gmail.com>
2017-02-12 16:36:03 +0100

    More content

---
+++ examples/e13.py
@@ -0,0 +1,11 @@
+import cv2
+from matplotlib import pyplot as plt
+
+# Load
+image_path = 'images/rabbit.jpg'
+image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
+# Histogram & plot
+plt.hist(image.ravel(), 256, [0,256]);
+plt.show()
+
