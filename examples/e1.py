4e329e8e89c21fafc6857ba42570e2dd13191d74
Rubén Crespo-Cano <rcrespocano@gmail.com>
2017-02-23 09:40:43 +0100

    Fix typo after overall reading

--- examples/e1.py
+++ examples/e1.py
@@ -13,7 +13,7 @@ image_copy = cv2.imread(image_copy_path)
 
 # Show
 cv2.imshow('Original', image)
-cv2.imshow('Copy', image)
+cv2.imshow('Copy', image_copy)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
